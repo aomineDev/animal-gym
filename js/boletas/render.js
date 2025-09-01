e9d2db8c0723333e25ddf23a870ff8c77789b15d
leo <leonardoelbaneado@gmail.com>
2025-09-01 01:04:08 -0500

    empleado y boleta

--- js/boletas/render.js
+++ js/boletas/render.js
@@ -1,11 +1,11 @@
 import { boletaCard } from "./componentes/boletaCard.js";
 //en el parametro de lafuncion le le llegara del json y del main
 export function renderBoleta(boleta, container) {
-  container.innerHtml = "";
+  container.innerHTML = "";
 
   //reccores todas las boletad
   boleta.forEach((element, i) => {
-    container.innerHtml += boletaCard(element);
+    container.innerHTML += boletaCard(element);
   });
 }
 //como ay muchas voletas va pintar una por una
