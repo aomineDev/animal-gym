eff3c5f783588d5b5fcf3b2bf473eeacf40864e8
Jhordan <calixto4211@gmail.com>
2025-09-22 08:41:19 -0500

    avance modales vista  clases

--- src/main/java/pe/edu/utp/animalGym/controller/web/ClaseController.java
+++ src/main/java/pe/edu/utp/animalGym/controller/web/ClaseController.java
@@ -21,34 +21,15 @@ import org.springframework.stereotype.Controller;
 public class ClaseController {
   @Autowired
   private ClaseServiceImpl service;
-  @GetMapping("/clase")
+
+  @GetMapping("/clases")
   public String clase(Model model) {
     model.addAttribute("title", "Animal GYM | Clase");
     model.addAttribute("content", "clase :: content");
-    model.addAttribute("modal", "clase :: modal");
-    model.addAttribute("activePage", "clase");
+    model.addAttribute("modal", "clase :: modals");
+    model.addAttribute("activePage", "clases");
     model.addAttribute("clases", service.findAll());
-    
+
     return "layout";
-    // List<Clase> clases = new ArrayList<>();
-
-    // Empleado empleado1 = new Empleado();
-    // empleado1.setNombre("Juan Pérez");
-    // Empleado empleado2 = new Empleado();
-    // empleado2.setNombre("María Gómez");
-
-    // clases.add(new Clase(1, "Yoga Avanzado", "Clase de relajación y flexibilidad", 20,
-    //     LocalDate.now().plusDays(1), LocalTime.of(9, 0), LocalTime.of(10, 0), 60,
-    //     "Programado", "Mejorar elasticidad", "Baja", "https://picsum.photos/400/250?random=1", empleado1));
-
-    // clases.add(new Clase(2, "Crossfit Intenso", "Entrenamiento de alta intensidad", 15,
-    //     LocalDate.now().plusDays(2), LocalTime.of(18, 0), LocalTime.of(19, 30), 90,
-    //     "En curso", "Mejorar fuerza y resistencia", "Alta", "https://picsum.photos/400/250?random=2", empleado2));
-
-    // clases.add(new Clase(3, "Zumba Energética", "Baile y cardio divertido", 25,
-    //     LocalDate.now().plusDays(3), LocalTime.of(10, 0), LocalTime.of(11, 0), 60,
-    //     "Finalizado", "Mejorar coordinación y cardio", "Media", "https://picsum.photos/400/250?random=3", empleado1));
-
-    // model.addAttribute("clases", clases);
   }
 }
