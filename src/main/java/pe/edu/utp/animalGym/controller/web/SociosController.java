b64acf3ec19fdcad951d5f7fb1eca44dd7777f4b
indexclever <u23240301@utp.edu.pe>
2025-09-24 11:04:12 -0500

    socios

--- src/main/java/pe/edu/utp/animalGym/controller/web/SociosController.java
+++ src/main/java/pe/edu/utp/animalGym/controller/web/SociosController.java
@@ -1,19 +1,28 @@
 package pe.edu.utp.animalGym.controller.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import pe.edu.utp.animalGym.repository.SocioRepository;
+import pe.edu.utp.animalGym.service.impl.SocioServiceImpl;
+
 @Controller
 public class SociosController {
+    
+    // @Autowired
+    // private SocioServiceImpl socio;
+    @Autowired
+    private SocioRepository socioRepository;
     @GetMapping("/socios")
-
     public String Socio(Model model) {
         model.addAttribute("content", "socios :: content");
 
         model.addAttribute("modal", "socios :: modal");
 
         model.addAttribute("activePage", "socios");
+        model.addAttribute("socios", socioRepository.findAll());
         return "layout";
     }
 }
--- src/main/java/pe/edu/utp/animalGym/model/Socio.java
+++ src/main/java/pe/edu/utp/animalGym/model/Socio.java
@@ -19,18 +19,20 @@ public class Socio extends Person {
     private int puntos;
     private double peso;
     private double altura;
+    private String imagen;
     private Membresia menbresia;
     private List<Rutina> rutinas;
 
     public Socio(Integer personaId, String dni, String nombre, String apellido, String telefono, String genero,
             String email, LocalDate fechaNacimiento, LocalDate fechaIngreso, LocalDate fechaVencimiento, boolean activo,
-            int puntos, double peso, double altura, Membresia membership, List<Rutina> rutinas) {
+            int puntos, double peso, double altura,String imagen, Membresia membership, List<Rutina> rutinas) {
         super(personaId, dni, nombre, apellido, telefono, genero, email, fechaNacimiento, fechaIngreso);
         this.fechaVencimineto = fechaVencimiento;
         this.activo = activo;
         this.puntos = puntos;
         this.peso = peso;
         this.altura = altura;
+        this.imagen = imagen;
         this.menbresia = membership;
         this.rutinas = rutinas;
     }
