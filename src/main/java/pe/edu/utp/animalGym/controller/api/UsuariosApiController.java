0051d99df7614feb8f2235194c67fc065508ec16
Jhordan <calixto4211@gmail.com>
2025-09-25 21:38:35 -0500

    correcion usuario api

--- src/main/java/pe/edu/utp/animalGym/controller/api/UsuariosApiController.java
+++ src/main/java/pe/edu/utp/animalGym/controller/api/UsuariosApiController.java
@@ -3,8 +3,11 @@ package pe.edu.utp.animalGym.controller.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +24,10 @@ public class UsuariosApiController {
     @GetMapping
     public ResponseEntity<List<Usuario>> findAll() {
         return ResponseEntity.ok(usuarioService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> save(@RequestBody Usuario usuario) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioService.save(usuario));
     }
 }
