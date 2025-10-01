15fdf3e89d62a39e51bcf73f0b9782c052991caa
Jhordan <calixto4211@gmail.com>
2025-10-01 13:38:43 -0500

    terminado rutina, thyme y js, solo falta detalles

--- src/main/resources/static/js/rutinas/events/modalEvents.js
+++ src/main/resources/static/js/rutinas/events/modalEvents.js
@@ -3,6 +3,18 @@ import { renderFilaSocio, renderTablaDetalle } from "../render.js";
 import { formCrearRutina, crearRutinaModal } from "../dom.js";
 import { resetFormModalClose, showToast } from "../../utils.js";
 
+let usuarios = [];
+
+async function fetchUsuarios() {
+  try {
+    const response = await fetch("/api/usuarios");
+    if (!response.ok) throw new Error("Error al obtener usuarios");
+    usuarios = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export const renderFilaEvents = () => {
   document.body.addEventListener("click", async (event) => {
     const fila = event.target.closest(".fila-rutina");
@@ -44,6 +56,8 @@ export const renderFilaEvents = () => {
 };
 
 export const crearRutinaEvents = () => {
+  fetchUsuarios();
+
   //reseteo del modal: Solo si cierra sin hacer cambios
   resetFormModalClose(crearRutinaModal, formCrearRutina);
 
@@ -82,7 +96,7 @@ export const crearRutinaEvents = () => {
 
       console.log(socioCompleto);
 
-      renderFilaSocio(socioCompleto);
+      renderFilaSocio(socioCompleto, usuarios);
       //renderTablaRutinaModal(socioCompleto);
 
       showToast("Rutina creado correctamente", 1);
@@ -104,6 +118,7 @@ export const crearRutinaEvents = () => {
 };
 
 export const editarRutinaEvents = () => {
+  fetchUsuarios();
   document.body.addEventListener("click", async (event) => {
     if (!event.target.classList.contains("btnEditarRutina")) return;
 
@@ -134,9 +149,45 @@ export const editarRutinaEvents = () => {
     let socioService = new Service("socios");
 
     try {
+      //actualizo la rutina
       let rutinaCompleta = await rutinaService.update(rutina, rutinaId);
+
+      //llamo al socio de esa rutina
       let socioIncompleto = await socioService.findById(socioId);
-      socioIncompleto.console.log("objeto en bd ", rutinaCompleta);
+
+      //busco la rutina del socio incompleto para actulizar
+      let index = socioIncompleto.rutinas.findIndex(
+        (r) => r.rutinaId === Number(rutinaId)
+      );
+      console.log(index);
+
+      socioIncompleto.rutinas[index] = rutinaCompleta;
+
+      let socioActualizado = await socioService.update(
+        socioIncompleto,
+        socioId
+      );
+
+      renderFilaSocio(socioActualizado, usuarios);
+
+      showToast("Rutina actualizada correctamente", 1);
+
+      // Cerrar el modal de creación
+      const bootstrapModal = bootstrap.Modal.getInstance(
+        document.getElementById(`editarRutina__${rutinaId}`)
+      );
+      bootstrapModal.hide();
+
+      // Abrir el modal de detalle
+      const detalleModal = new bootstrap.Modal(
+        document.getElementById(`detalleSocioModal__${socioId}`)
+      );
+      detalleModal.show();
+
+      console.log(
+        "Socio actualizado con rutina reemplazada: ",
+        socioActualizado
+      );
     } catch (err) {
       console.error("Error al actualizar rutina", err);
       showToast("Error al actualizar rutina", 2);
@@ -145,6 +196,7 @@ export const editarRutinaEvents = () => {
 };
 
 export const eliminarRutinaEvents = () => {
+  fetchUsuarios();
   document.body.addEventListener("click", async (event) => {
     if (!event.target.classList.contains("btnConfirmarEliminar")) return;
 
@@ -177,7 +229,7 @@ export const eliminarRutinaEvents = () => {
         console.log("eliminado bien");
 
         //renderizo
-        renderFilaSocio(socioActualizado);
+        renderFilaSocio(socioActualizado, usuarios);
         //renderTablaRutinaModal(socioActualizado);
         showToast("Rutina eliminada correctamente", 1);
 
--- src/main/resources/static/js/rutinas/render.js
+++ src/main/resources/static/js/rutinas/render.js
@@ -1,4 +1,4 @@
-export function renderFilaSocio(socio) {
+export function renderFilaSocio(socio, usuarios) {
   const filaHtml = /*html*/ `
     <tr id="socio-fila-${socio.personaId}">
       <td>${socio.nombre}</td>
@@ -109,7 +109,7 @@ export function renderFilaSocio(socio) {
                                     </button>
                                     <ul class="dropdown-menu">
                                       <li>
-                                        <a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#">
+                                        <a class="dropdown-item" href="#" data-bs-toggle="modal" data-bs-target="#editarRutina__${rutina.rutinaId}" data-socio-id="${socio.personaId}">
                                           <i class="bi bi-pencil me-2"></i>Editar
                                         </a>
                                       </li>
@@ -197,6 +197,92 @@ export function renderFilaSocio(socio) {
     )
     .join("");
 
+  // --- Modal Editar ---
+  const modalEditarHtml = socio.rutinas
+    .map(
+      (rutina) => /*html*/ `
+        <div class="modal fade" id="editarRutina__${
+          rutina.rutinaId
+        }" tabindex="-1" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content rounded-4">
+              <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Editar Rutina</h1> <button type="button"
+                  class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <form class="row needs-validation editarRutinaForm" novalidate>
+
+                  <!-- nombre-->
+                  <div class="mb-3 col-md-6"> <label for="nombre" class="form-label">Nombre de la rutina</label>
+                    <input type="text" name="nombre" class="form-control" value="${
+                      rutina.nombre
+                    }" required>
+                  </div>
+
+                  <!-- Entrenador -->
+                  <div class="mb-3 col-md-6">
+                    <label for="entrenador" class="form-label">Entrenador</label>
+                    <select name="entrenador" class="form-select" required>
+                      <option value="">Seleccione un entrenador</option>
+                      ${usuarios
+                        .filter((u) => u.rol.rolId === 2)
+                        .map(
+                          (u) => `
+                            <option value="${u.persona.personaId}" 
+                              ${
+                                u.persona.personaId ===
+                                rutina.empleado.personaId
+                                  ? "selected"
+                                  : ""
+                              }>
+                              ${u.persona.nombre} ${u.persona.apellido}
+                            </option>`
+                        )
+                        .join("")}
+                    </select>
+                  </div>
+
+                  <!-- descripcion -->
+                  <div class="mb-3 col-md-6"> <label for="descripcion" class="form-label">Descripción</label>
+                    <textarea name="descripcion" class="form-control" rows="3"
+                      placeholder="Escribe una breve descripción de la rutina"
+                      required>${rutina.descripcion}</textarea>
+                  </div>
+
+                  <!-- objetivo -->
+                  <div class="mb-3 col-md-6"> <label for="objetivo" class="form-label">Objetivo</label>
+                    <textarea name="objetivo" class="form-control" rows="3"
+                      placeholder="Escribe el objetivo de la rutina" required>${
+                        rutina.objetivo
+                      }</textarea>
+                  </div>
+
+                  <!-- fechaInicio -->
+                  <div class="mb-3 col-md-6"> <label for="fechaInicio" class="form-label">Fecha Inicio</label> <input
+                      type="date" name="fechaInicio" class="form-control" value="${
+                        rutina.fechaInicio
+                      }" required>
+                  </div>
+
+                  <!-- fechaFin -->
+                  <div class="mb-3 col-md-6"> <label for="fechaFin" class="form-label">Fecha Fin</label> <input
+                      type="date" name="fechaFin" class="form-control" value="${
+                        rutina.fechaFin
+                      }" required> </div>
+                  <button type="submit" class="btn btn-primary col-5 ms-3 btnEditarRutina"
+                    data-socio-id="${socio.personaId}" data-id="${
+        rutina.rutinaId
+      }">Editar rutina</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>    
+    `
+    )
+    .join("");
+
   // Inyectar o reemplazar la fila en la tabla principal
   const filaExistente = document.querySelector(
     `#socio-fila-${socio.personaId}`
@@ -216,6 +302,15 @@ export function renderFilaSocio(socio) {
     modalExistente.outerHTML = modalHtml;
   } else {
     document.body.insertAdjacentHTML("beforeend", modalHtml);
+  }
+
+  const modalEditarExistente = document.querySelector(
+    `#editarRutina__${socio.personaId}`
+  );
+  if (modalEditarExistente) {
+    modalEditarExistente.outerHTML = modalEditarHtml;
+  } else {
+    document.body.insertAdjacentHTML("beforeend", modalEditarHtml);
   }
 
   const modalEliminarExistente = document.querySelector(
