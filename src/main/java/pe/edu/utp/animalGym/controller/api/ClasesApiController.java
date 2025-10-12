714a073dca90f05994794ec5b0e80cacaac7c557
Jhordan <calixto4211@gmail.com>
2025-10-12 13:26:02 -0500

    merge

--- src/main/java/pe/edu/utp/animalGym/controller/api/ClasesApiController.java
+++ src/main/java/pe/edu/utp/animalGym/controller/api/ClasesApiController.java
@@ -1,19 +1,12 @@
 package pe.edu.utp.animalGym.controller.api;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
 import pe.edu.utp.animalGym.model.Clase;
 import pe.edu.utp.animalGym.service.ClaseService;
 
